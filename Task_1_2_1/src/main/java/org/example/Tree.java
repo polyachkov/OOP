04f517c23cb4969a4dcc8f9a7e22c10eec155b4d
Dmitriy Polyachkov <d.polyachkov@g.nsu.ru>
2023-10-18 02:23:56 +0700

    getters, setters, addchild

--- Task_1_2_1/src/main/java/org/example/Tree.java
+++ Task_1_2_1/src/main/java/org/example/Tree.java
@@ -4,13 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tree<DataType> {
-    DataType data;
-    List<Tree<DataType>> children;
-    public void Tree(DataType data) {
+    private Tree<DataType> parent = null;
+    private DataType data;
+    private List<Tree<DataType>> children;
+
+    public Tree(DataType data) {
         this.data = data;
         children = new ArrayList<>();
     }
 
-    
+    public Tree<DataType> getParent() {
+        return parent;
+    }
+
+    public DataType getData() {
+        return data;
+    }
+
+    public List<Tree<DataType>> getChildren() {
+        return children;
+    }
+
+    public void setParent(Tree<DataType> parent) {
+        this.parent = parent;
+    }
+
+    public void setData(DataType data) {
+        this.data = data;
+    }
+
+    public void setChildren(List<Tree<DataType>> children) {
+        this.children = children;
+    }
+
+
+    public Tree<DataType> addChild(DataType data) {
+        Tree<DataType> child = new Tree<>(data);
+        child.setParent(this);
+        return child;
+    }
 
 }
