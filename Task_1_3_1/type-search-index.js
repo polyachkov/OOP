ba89ac90fed163d56d83f36f6623c68f9a64ee53
polyachkov <polyachkov@users.noreply.github.com>
2023-11-30 08:24:31 +0000

    Deploying to gh-pages from @ polyachkov/OOP@1a2744cafa4d773c4631de99989be9e6d72e801c 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"SubStringsFinder"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"SubStringFinder"}]
