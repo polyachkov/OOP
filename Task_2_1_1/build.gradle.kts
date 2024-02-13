9f1be4518e0ec2ef23fd52f16504bcb9cb784d93
polyachkov <d.polyachkov@g.nsu.ru>
2024-02-13 23:35:40 +0700

    Update build.gradle.kts

--- Task_2_1_1/build.gradle.kts
+++ Task_2_1_1/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
 }
 
 group = "org.example"
@@ -10,6 +11,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.jfree:jfreechart:1.5.3")
@@ -17,4 +21,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
