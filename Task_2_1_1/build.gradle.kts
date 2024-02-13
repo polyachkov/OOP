4e42595af05dd8c535962db7adde0ffd169efc6b
polyachkov <d.polyachkov@g.nsu.ru>
2024-02-13 23:46:32 +0700

    Update build.gradle.kts

--- Task_2_1_1/build.gradle.kts
+++ Task_2_1_1/build.gradle.kts
@@ -12,8 +12,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("org.jetbrains:annotations:24.0.0")
-    implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.jfree:jfreechart:1.5.3")
