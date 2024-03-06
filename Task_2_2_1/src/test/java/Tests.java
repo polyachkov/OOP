236b637b3667c7dceb4d208c43656bdb1c0e709c
polyachkov <d.polyachkov@g.nsu.ru>
2024-03-06 23:55:44 +0700

    some tests

---
+++ Task_2_2_1/src/test/java/Tests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tests {
+    @Test
+    public void myTest() {
+        assertTrue(true);
+    }
+}
