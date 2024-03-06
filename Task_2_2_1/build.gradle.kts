1ed3690bc1cdf3da23c313b4097cd7c464409437
polyachkov <d.polyachkov@g.nsu.ru>
2024-03-06 23:59:44 +0700

    jUnit

--- Task_2_2_1/build.gradle.kts
+++ Task_2_2_1/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
 }
 
 group = "org.example"
@@ -10,11 +11,19 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:24.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jfree:jfreechart:1.5.3")
     implementation("com.google.code.gson:gson:2.9.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
